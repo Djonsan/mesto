fce7d193cbbfdee41842f7d2fc3b91e4402bd659
Evgeniy Tolochkin <djon-rap@yandex.ru>
2022-06-03 23:44:26 +1000

    ПР 8

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,58 @@
-const path = require('path');
+  const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    main: './src/index.js'
-  },
+  entry: { main: './src/pages/index.js' },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'main.js',
-    publicPath: '',
+        publicPath: '',
   },
   mode: 'development',
   devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
-    open: true,
+    static: path.resolve(__dirname, './dist'),
     compress: true,
-    port: 8080
+    port: 8080,
+    open: true,
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js$/,
         use: 'babel-loader',
-        exclude: '/node_modules/'
+        exclude: '/node_modules/',
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[hash][ext]',
+        },
       },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
+          loader: 'css-loader',
+          options: { importLoaders: 1 },
+        },
+        'postcss-loader']
       },
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
+      template: './src/index.html',
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
-
   ]
-}
+};
